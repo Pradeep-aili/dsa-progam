145624b08667710661d64b0d3fccf0ea18f15149
Pradeep Aili <ailipradeep5@gmail.com>
2026-09-04 23:57:19 +0530

    added the quick sort progam

--- src/com/tcs/linkedlist/SingleLinkedList.java
+++ src/com/tcs/linkedlist/SingleLinkedList.java
@@ -1,78 +1,66 @@
 package com.tcs.linkedlist;
 
-class Node
-{
+class Node {
 	int value;
 	Node next;
 }
 
-
 public class SingleLinkedList {
-	Node head,tail;
-	public void creation(int data)
-	{
+	Node head, tail;
+
+	public void creation(int data) {
 		Node node = new Node();
 		node.value = data;
 		node.next = null;
 		head = tail = node;
-		System.out.print(node.value+" ");
+		System.out.print(node.value + " ");
 	}
-	
-	public void insertion(int index,int data)
-	{
+
+	public void insertion(int index, int data) {
 		Node node = new Node();
-		node.value=data;
-		if(index == 0)
-		{
-			node.next=head;
-			head=node;
-		}
-		else {
-			node.next=null;
-			tail=tail.next=node;
+		node.value = data;
+		if (index == 0) {
+			node.next = head;
+			head = node;
+		} else {
+			node.next = null;
+			tail = tail.next = node;
 		}
 	}
-	
-	public void traverse()
-	{
+
+	public void traverse() {
 		System.out.println("*****Forward Traverse*****");
-		Node temp=head;
-		while (temp!=null) {
-			System.out.print(temp.value+" ");
-			temp=temp.next;
+		Node temp = head;
+		while (temp != null) {
+			System.out.print(temp.value + " ");
+			temp = temp.next;
 		}
 	}
-	
-	public void reverseTraverse()
-	{
-		Node temp=reverse();
-		while (temp!=null) {
-			System.out.print(temp.value+" ");
-			temp=temp.next;
+
+	public void reverseTraverse() {
+		Node temp = reverse();
+		while (temp != null) {
+			System.out.print(temp.value + " ");
+			temp = temp.next;
 		}
 	}
-	
-	public Node reverse()
-	{
-		Node prev=null, mid=null,cn=head; 
-		while(cn!=null)
-		{
-			prev=mid;
-			mid=cn;
-			cn=cn.next;
-			mid.next=prev;
+
+	public Node reverse() {
+		Node prev = null, mid = null, cn = head;
+		while (cn != null) {
+			prev = mid;
+			mid = cn;
+			cn = cn.next;
+			mid.next = prev;
 		}
 		return mid;
 	}
-	
-	public void searching(int searchValue)
-	{
+
+	public void searching(int searchValue) {
 		System.out.println("*****Searching*****");
-		Node temp=head;
-		while(temp!=null)
-		{
-			if(temp.value==searchValue)
-			{
+		Node temp = head;
+		while (temp != null) {
+			if (temp.value == searchValue) {
 				System.out.println("Element found");
 				return;
 			}
@@ -80,7 +68,7 @@ public class SingleLinkedList {
 		}
 		System.out.println("Element not found");
 	}
-	
+
 	public static void main(String[] args) {
 		SingleLinkedList linkedList = new SingleLinkedList();
 		linkedList.creation(10);
@@ -88,12 +76,12 @@ public class SingleLinkedList {
 		linkedList.insertion(2, 30);
 		linkedList.insertion(3, 40);
 		linkedList.insertion(5, 50);
-		
+
 		linkedList.traverse();
-		
+
 		System.out.println("\n******Reverse Traverse*****");
 		linkedList.reverseTraverse();
-		
+
 		linkedList.searching(50);
 	}
 }
